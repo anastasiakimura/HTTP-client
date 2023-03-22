ec14cb6c54c89a7156e356c6d7c102bee647c0ea
anastasiakimura <akinekimura@gmail.com>
2023-03-22 21:34:28 +0500

    создала http-клиент

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 4444  # The port used by the server
+HOST = "www.google.ru"  # The server's hostname or IP address
+PORT = 80  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(10)
     s.connect((HOST, PORT))
-
-    message = input()
-
-    while message != 'stop':
-        s.sendall(bytes(message.encode('utf-8')))
-        data = s.recv(1024)
-        print(f"Received {data.decode('utf-8')!r}")
-        message = input()
+    request = b"GET / HTTP/1.1\r\nHost:www.google.ru\r\n\r\n"
+    sent = 0
+    while sent < len(request):
+        sent = sent + s.send(request[sent:])  # Send a portion of 'request', starting from 'sent' byte
+    response = b""
+    try:
+        while True:
+            test_recv = s.recv(4096)
+            if len(test_recv) == 0:
+                break
+            response = response + test_recv
+    except socket.timeout as e:
+        print("Time out!")
+    s.send(b"GET / HTTP/1.1\r\nHost:www.google.ru\r\nConnection: close\r\n\r\n")
