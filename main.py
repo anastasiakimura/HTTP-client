555d5040d3f5e4af3c35ceff098785070cc238ae
anastasiakimura <akinekimura@gmail.com>
2023-03-22 21:47:16 +0500

    сделала декодировку полученных в ответ данных

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "www.google.ru"  # The server's hostname or IP address
-PORT = 80  # The port used by the server
+HOST = "www.google.ru"
+PORT = 80
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.settimeout(10)
@@ -9,14 +9,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     request = b"GET / HTTP/1.1\r\nHost:www.google.ru\r\n\r\n"
     sent = 0
     while sent < len(request):
-        sent = sent + s.send(request[sent:])  # Send a portion of 'request', starting from 'sent' byte
+        sent = sent + s.send(request[sent:])
     response = b""
     try:
         while True:
-            test_recv = s.recv(4096)
-            if len(test_recv) == 0:
-                break
-            response = response + test_recv
+            response = response + s.recv(4096)
     except socket.timeout as e:
         print("Time out!")
     s.send(b"GET / HTTP/1.1\r\nHost:www.google.ru\r\nConnection: close\r\n\r\n")
+    print(response.decode('Windows-1251'))
